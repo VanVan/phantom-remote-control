6b097db3644db7bd2fa4e195e4e82d78737905b8
Wandrille RONCE <w@ndrille.fr>
2019-09-06 19:00:00 +0200

    Create basic HTTP server

--- index.js
+++ index.js
@@ -14,5 +14,29 @@ if (config.debug) {
 	const dotenv = require('dotenv');
 	dotenv.config();
 }
-var ssdp = require('node-ssdp').Client;
+var ssdp = require('node-ssdp').Client
+var parseUri = require('parseuri');
 var client = new ssdp();
+var http = require('http');
+var url = require('url');
+var request = require('request');
+
+
+create_http_server();
+
+
+
+
+
+function create_http_server() {
+
+	http.createServer(function (req, res) {
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+
+	var q = url.parse(req.url, true).query;
+
+	
+	
+	}).listen(config.http_local_port);
+}
