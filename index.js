a4672dffaecf945c007841fffa993416e4f63123
Wandrille RONCE <w@ndrille.fr>
2019-09-06 19:00:00 +0200

    Creating main project file with config.json

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * phantom-remote-control
+ * HTTP Adapter to remote control Devialet Phantom sound system
+ *               (Play, Pause, Volume etc.)
+ * 
+ * @french Contrôlez les enceintes Devialet via une API HTTP. (Lecture, Pause, Volume etc.)
+ * @package phantom-remote-control
+ * @author https://github.com/VanVan
+ */
+
+
+const config = require('./config.json');
